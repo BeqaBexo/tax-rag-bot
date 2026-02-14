b511e5639844657ff0a31d2ef9bcbd27062cfb09
Bek Tsaava <beqacaava1997@gmail.com>
2026-02-15 00:14:53 +0400

    correct app file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ Streamlit Web აპლიკაცია
 import sys
 from pathlib import Path
 project_root = Path(__file__).resolve().parent
+sys.path.insert(0, str(project_root / "src"))
 sys.path.insert(0, str(project_root))
 
 import streamlit as st
